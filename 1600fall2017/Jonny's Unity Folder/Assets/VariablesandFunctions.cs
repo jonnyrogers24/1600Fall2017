44308cbdb113cac2ee7020a2c0345db24dea9814
Unknown <jonnyrogersanimation@gmail.com>
2017-09-12 08:58:36 -0600

    Playing around with Unity and VS

---
+++ 1600fall2017/Jonny's Unity Folder/Assets/VariablesandFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesandFunctions : MonoBehaviour {
+
+	int myInt = 100;
+
+	// Use this for initialization
+	void Start () {
+		myInt = 200;
+		Debug.Log (myInt / 2);
+	}
+	
+
+}
