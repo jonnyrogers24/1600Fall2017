81deb6cfb75394f3723a8d942cc0694079d147d4
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 17:23:01 -0600

    Created script for Pythagorean Theorem
    this script uses the pothagorean theorem formula which is a^2 + b^2 = c^2

---
+++ 1600fall2017/Assets/PythagoreanTheorem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script uses the pothagorean theorem formula which is a^2 + b^2 = c^2
+public class PythagoreanTheorem : MonoBehaviour {
+
+	// Use this for initialization
+	public int sideA;
+	public int sideB;
+	public int hypotenuseC;
+	private int aSquared;
+	private int bSquared;
+	private int cSquared;
+
+	// Update is called once per frame
+	void Update () {
+		aSquared = sideA * sideA;
+		bSquared = sideB * sideB;
+		cSquared = aSquared + bSquared;
+		hypotenuseC = cSquared;
+			print("YOU DID IT");
+	}
+}
