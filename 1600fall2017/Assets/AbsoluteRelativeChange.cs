4d2de8dc47a2672979a9b3f7f52b664c7cd3e9ba
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 17:04:10 -0600

    Created script to calculate Absolute & Relative Change
    this can be used to calculate the value change of an item and the percent lost or gained.

---
+++ 1600fall2017/Assets/AbsoluteRelativeChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this can be used to calculate the value change of an item and the percent lost or gained. 
+public class AbsoluteRelativeChange : MonoBehaviour {
+
+public int newValue;
+public int oldValue;
+public float absoluteChange;
+public float relativeChange;
+
+	
+	// Update is called once per frame
+	void Update () {
+		absoluteChange = newValue - oldValue;
+		relativeChange = (absoluteChange / oldValue) * .100f;
+	}
+}
