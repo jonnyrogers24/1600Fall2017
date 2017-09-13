80e249dcc8cb37861dc71b839268eec1914ed0d2
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-09-13 11:32:16 -0600

    Learn if statements and else clause. Also learn math operators

--- 1600fall2017/Assets/ifStatement.cs
+++ 1600fall2017/Assets/ifStatement.cs
@@ -7,14 +7,24 @@ public class ifStatement : MonoBehaviour {
 
 	public Text input;
 
-	public string password = "france2012";
+	public bool canPlayGame = false;
+	public string password = "hi5";
 
 	void Update () {
 		if(input.text == password) {
 			print("You know the password.");
+			canPlayGame = true;
+		} else {
+			print("The password is incorrect");
 		}
+
+		if(canPlayGame) {
+			print("Playing Game"); 
+		}	else {
+			print("Can't play yet, Enter Password");
+		}	
+
 	}
-	
 
 }
 	
