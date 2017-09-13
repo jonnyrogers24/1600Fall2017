667cdc3c0458055f6337e8b0c25e33af8ad907a7
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 17:59:07 -0600

    Created script to calculate time when given distance and rate

---
+++ 1600fall2017/Assets/TimeCalc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script is to calculate how long it would take me to travel a certain distance and a certain rate.
+
+public class TimeCalc : MonoBehaviour {
+
+	public int miles;
+	public int mph;
+	public int pitStopTime;
+	public double hours;
+	
+	// Update is called once per frame
+	void Update () {
+		hours = (miles / mph) + pitStopTime;
+	}
+}
