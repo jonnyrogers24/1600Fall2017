4988fa5da0369c4df488c71f779623dd6d9f9463
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 16:46:34 -0600

    created a script that calculates the volume of a cube.

---
+++ 1600fall2017/Assets/CubeVolume.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeVolume : MonoBehaviour {
+
+public int cubeWidth;
+
+public int cubeHeighth;
+
+public int cubeLength;
+
+public int totalVolume; 
+
+	
+	// Update is called once per frame
+	void Update () {
+		totalVolume = cubeWidth * cubeHeighth * cubeLength;
+	}
+}
