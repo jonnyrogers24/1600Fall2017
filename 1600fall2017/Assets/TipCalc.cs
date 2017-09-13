3246c4f9d3ec60202be2774f0f8975d13d0c7e5e
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 16:24:01 -0600

    worked out how to make a tip calculator using math operators

---
+++ 1600fall2017/Assets/TipCalc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalc : MonoBehaviour {
+
+	public float mealCost;
+	private float tipPercent = 0.15f; 
+	public float tipAmount; 
+	public float totalBill;
+
+	
+	// Update is called once per frame
+	void Update () {
+		tipAmount = mealCost * tipPercent;
+		 totalBill= mealCost + tipAmount;
+	}
+}
---
+++ 1600fall2017/Jonny's Unity Folder/Assets/MathOperators.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour {
+
+float totalCost = 100.00f; 
+float tipPercent = .15f;
+float tipAmount;
+	}
+	
