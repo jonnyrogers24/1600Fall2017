c9a6ee1a47b9b5d954825a16899b1c61b094cb09
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 16:46:09 -0600

    Created a script that calculates square footage

---
+++ 1600fall2017/Assets/SquareFeet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareFeet : MonoBehaviour {
+
+public float num1;
+public float num2;
+public float squareFeet;
+
+	
+	// Update is called once per frame
+	void Update () {
+		squareFeet = num1 * num2;
+	}
+}
