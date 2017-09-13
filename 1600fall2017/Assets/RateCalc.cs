411332a2bbd3401c188cef5b90149dba936a2f59
Unknown <jonnyrogersanimation@gmail.com>
2017-09-13 17:58:34 -0600

    Created script to calculate rate when given distance and time

---
+++ 1600fall2017/Assets/RateCalc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script is the rate = distance / time formula. Rate will be calculated hen given distance and time. 
+public class RateCalc : MonoBehaviour {
+
+	public float milesTraveled;
+	public float hoursTraveled;
+	public float mph;
+		
+	// Update is called once per frame
+	void Update () {
+		mph = milesTraveled / hoursTraveled;
+		
+	}
+}
