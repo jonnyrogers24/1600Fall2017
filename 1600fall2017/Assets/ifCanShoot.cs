e874730eeee791c08f4d80902b85702c7ff27c72
Unknown <jonnyrogersanimation@gmail.com>
2017-09-15 10:18:19 -0600

    Created an if/esle script to let you shoot or not shoot
    toggle added for safety on/off switch

---
+++ 1600fall2017/Assets/ifCanShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifCanShoot : MonoBehaviour {
+
+	public Toggle safety;
+	
+	// Update is called once per frame
+	void Update () {
+		if(safety.isOn)
+		{
+			print("Safety Off, Shoot Gun");
+		}else{
+			print("Safety On, Can't Shoot Gun");
+		} 
+
+	}
+}
