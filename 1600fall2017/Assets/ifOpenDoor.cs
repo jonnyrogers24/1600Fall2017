e06c600301b9017c33e1b3a7891e523572a96a1d
Unknown <jonnyrogersanimation@gmail.com>
2017-09-15 10:01:40 -0600

    Created script granting access to enter door using pin.

---
+++ 1600fall2017/Assets/ifOpenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifOpenDoor : MonoBehaviour {
+public Text input;
+public bool canOpenDoor = false;
+public string pin = "1212"; 
+	
+	void Update () {
+		if(input.text == pin) {
+			print("You May Enter");
+			canOpenDoor = true; 
+		}else{
+			print("Enter 4-digit Pin");
+		}
+		
+	}
+}
