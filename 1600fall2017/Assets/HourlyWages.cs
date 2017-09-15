bcab81410544f6b402e7154269c4c07fa0d59526
Unknown <jonnyrogersanimation@gmail.com>
2017-09-15 09:20:15 -0600

    Created a script that calculated paycheck based on hours and wages/hour.

---
+++ 1600fall2017/Assets/HourlyWages.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HourlyWages : MonoBehaviour {
+
+public float payPerHour;
+public float hoursWorked;
+public float totalPay;
+	
+	// Update is called once per frame
+	void Update () {
+		totalPay = hoursWorked * payPerHour;
+		print("CA-CHING! WE RICH BABY");
+	}
+}
