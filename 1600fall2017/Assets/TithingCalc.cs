766da6c5a070032d652d0142003d82f44d8cdb49
Unknown <jonnyrogersanimation@gmail.com>
2017-09-15 09:31:18 -0600

    Creted a simple script to calculate tithing.

---
+++ 1600fall2017/Assets/TithingCalc.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TithingCalc : MonoBehaviour {
+
+public float payCheckAmount;
+private float tithing = .10f;
+public float totalTithing;
+	
+	// Update is called once per frame
+	void Update () {
+		totalTithing = payCheckAmount * tithing;
+		print("Stay righteous Brother/Sister");
+	}
+}
