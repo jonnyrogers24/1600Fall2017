86b9b59d8054d1c3bff5fccfc4edba3827d9e4e9
Unknown <jonnyrogersanimation@gmail.com>
2017-09-18 21:25:31 -0600

    Made a if script where user has to chose between the red or blue wire
    red wire disarmed bomb, blue wire made bomb explode.

---
+++ 1600fall2017/Assets/ifRedWireBlueWire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifRedWireBlueWire : MonoBehaviour {
+public Text input; 
+
+public bool bombDisarmed = false; 
+
+private string rightWire = "red";
+private string wrongWire = "blue";
+	// Update is called once per frame
+	void Update () {
+		if(input.text == rightWire) {
+			print("Bomb Disarmed");
+			bombDisarmed = true; 
+		}
+		if(input.text == wrongWire) {
+			print("BOOM! You're Dead!");
+		}
+	}
+}
