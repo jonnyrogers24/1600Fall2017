9f05fa46f87b0cefd9c7dfc0ba5b597093b50aa9
Unknown <jonnyrogersanimation@gmail.com>
2017-09-27 20:28:31 -0600

    ifMillion Dolar Question

---
+++ 1600fall2017/Assets/ifMillionDollarQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ifMillionDollarQuestion : MonoBehaviour {
+
+public Text input;
+public bool canWin = false; 
+public string number = "2";
+
+void update () {
+		if(input.text == number) {
+			print("Congrats! You Won A Million Chrute Bucks!!"); 
+			canWin = true; 
+		} else {
+			print("Sorry, you lost. Thanks for playing!");
+		}
+	}		
+}
