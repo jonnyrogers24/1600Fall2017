b2e07e7e99ae7e095a3bd3f72e6869ccacce9727
Unknown <jonnyrogersanimation@gmail.com>
2017-09-27 22:09:43 -0600

    Switch Cars Script w/Buttons

---
+++ 1600fall2017/Assets/SwitchCars.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCars : MonoBehaviour {
+		public string[] cars;
+	
+	// Update is called once per frame
+	public void MyCarSwitch (int i) {
+		switch (cars[i])
+		{
+			case "Lamborghini":
+			 print("Lamborghini");
+			 break;
+
+			case "Ferrari":
+				print("Ferrari");
+				break;
+			
+			case "Koenigsegg Agera R":
+				print("Koenigsegg Agera R");
+				break;
+			
+			case "Toyota 4-Runner":
+				print("Sorry Mr. Rogers, but your wife took that one grocery shopping. Lets pick another car!");
+				break;
+
+		}
+	}	
+}
