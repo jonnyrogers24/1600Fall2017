1926639a7d21f80bee0705157322b07ba73ac605
Unknown <jonnyrogersanimation@gmail.com>
2017-09-27 20:59:02 -0600

    Switch script to switch drinks

---
+++ 1600fall2017/Assets/SwitchDrinks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchDrinks : MonoBehaviour {
+
+	public string[] drinks; 
+	
+	// Update is called once per frame
+	public void MyDrinkSwitch (int i) {
+		switch (drinks[i])
+		{
+			case "Soda Pop":
+				print("Soda Pop");
+				break; 
+
+			case "Juice":
+				print("Juice");
+				break;
+
+			case "Water":
+				print("Water");
+				break;
+			
+			default:
+				print("You have no other drinks");
+				break;
+
+		}
+	}		
+}
---
+++ 1600fall2017/Assets/WeaponSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+
+	public string[] weapons;
+
+	public void MyWeaponSwitch (int i){
+		switch (weapons[i])
+		{
+			case "Missile":
+				print("Missile");
+				break;
+
+			case "Plasma":
+				print("Plasma");
+				break;
+				
+			case "Gun":
+				print("Gun");
+				break;
+			
+			default:
+			 print("You have no other weapons.");
+			 break;
+		}
+	}
+} 
