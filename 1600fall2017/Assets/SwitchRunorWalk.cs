350fc7ac9d71ecbcb949636d2ea1b68e79223f31
Unknown <jonnyrogersanimation@gmail.com>
2017-09-27 22:48:40 -0600

    Switch Run or Walk Script

---
+++ 1600fall2017/Assets/SwitchRunorWalk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchRunorWalk : MonoBehaviour {
+
+	public string[] Action;
+
+	public void MyActionsSwitch (int i) {
+		switch(Action[i])
+		{
+			case "Running":
+				print("Running");
+				break;
+
+			case "Walking":
+				print("Walking");
+				break;
+			
+			case "Nothing":
+				print("You Lazy Bum");
+				break;
+		}
+			
+	}
+}
