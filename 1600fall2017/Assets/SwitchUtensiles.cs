14edc7cb582d81ab2750edae1127aee0d59845ad
Unknown <jonnyrogersanimation@gmail.com>
2017-09-27 22:28:04 -0600

    Switch Utensiles Script

---
+++ 1600fall2017/Assets/SwitchUtensiles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchUtensiles : MonoBehaviour {
+
+	public string[] Utensiles;
+
+	public void MyUtensilesSwitch (int i) {
+		switch (Utensiles[i])
+		{
+			case "Fork":
+				print("Fork");
+				break;
+			
+			case "Spoon":
+				print("Spoon");
+				break;
+			
+			case "Knife":
+				print("Knife");
+				break;
+		}
+	}
+
+}
