3fcda47098883101a1e4803a0b22ce53282c5ceb
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-06 11:43:20 -0600

    VoidFunctions

---
+++ 1600fall2017/Assets/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+public string[] foxFood = {"Banana", "Apples", "Grapes", "Chicken", "Turkey", "Cake"};
+public int[] foodCount = {10, 2, 30, 1, 2, 1};
+public virtual void Start (){
+
+	for (int i = 0; i < foxFood.Length; i++)
+	{
+		Eat(foxFood[i], foodCount[i]);
+	}
+
+	Sleep();
+
+}
+
+void Die (){
+	print(this.name + "Dies");
+}
+
+void Eat (string food, int amount) {
+	print(this.name + "likes to eat" + food);
+	print(this.name + "ate" +amount);
+	}
+
+void Sleep () {
+	print(this.name + "sleeps");
+}
+
+}
