36db5d0862c3a2a2bcac9ad78954f86563144c87
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-09 11:52:13 -0600

    Dog w Functions and Returns

---
+++ 1600fall2017/Assets/Scripts/Snack Closet/Dogs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dogs : MonoBehaviour {
+
+
+	void Start () {
+		myDog = ReturnNum (lengthInches, weightPounds);
+	 
+	}
+	public int mydog = 1;
+
+	int lengthInches = 20;
+
+	int weightPounds = 35;	 
+	
+		
+}
+
