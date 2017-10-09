96a5db2d15421c309e784f9bc9c44ee234f7f1a5
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-09 11:34:22 -0600

    Functions w/ Returns

---
+++ 1600fall2017/Assets/Returns.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Returns : MonoBehaviour {
+	void Start () {
+		myScore = ReturnNum (health, ammo);
+		myPassword = ReturnPassword("hello");
+	}
+	
+	public int myScore = 100; 
+
+	public int health ; 
+	public int ammo;
+
+	public string myPassword = "Amanda224";
+	int ReturnNum (int _num, int _num2) {
+		return _num + _num2;
+	}
+
+	string ReturnPassword (string _login) {
+		if(_login == "hello"){
+			return "Password Correct";
+		}else{
+			return "Incorrect";
+		}
+	}
+}
