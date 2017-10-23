f4e19f73c05dfc127de479370eb7516085d7eb1a
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-23 11:56:31 -0600

    Health Bar

---
+++ 1600fall2017/Assets/FinalProject/Scripts/UI/UI_Health_Bar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Health_Bar : MonoBehaviour {
+
+	public Image healthBar; 
+	public float barTime = 0.1f;
+
+	void OnTriggerEnter() {
+		StartCoroutine(UpdateBar());
+	}
+
+	IEnumerator UpdateBar () {
+		while (healthBar.fillAmount < 1)
+		{
+			healthBar.fillAmount 
+			+= barTime; 
+			yield return new WaitForSeconds(barTime);
+		}
+	}	
+
+}
