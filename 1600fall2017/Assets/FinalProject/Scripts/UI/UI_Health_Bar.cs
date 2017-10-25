79b8ee25289057a442c1f73fa7660c2e612753cc
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-25 11:55:33 -0600

    fill amount health bar

--- 1600fall2017/Assets/FinalProject/Scripts/UI/UI_Health_Bar.cs
+++ 1600fall2017/Assets/FinalProject/Scripts/UI/UI_Health_Bar.cs
@@ -7,18 +7,46 @@ public class UI_Health_Bar : MonoBehaviour {
 
 	public Image healthBar; 
 	public float barTime = 0.1f;
+	public float powerLevel = 0.1f; 
+	public float amountToAdd = 0.01f;
 
-	void OnTriggerEnter() {
-		StartCoroutine(UpdateBar());
+	public enum PowerUpType
+	{
+		PowerUp,
+		PowerDown
 	}
 
-	IEnumerator UpdateBar () {
+	public PowerUpType powerUp; 
+	void OnTriggerEnter() {
+
+		switch (powerUp)
+		{
+			case PowerUpType.PowerUp:
+				StartCoroutine(PowerUpBar());
+			break;
+
+			case PowerUpType.PowerDown:
+				StartCoroutine(PowerDownBar());
+			break; 
+		}
+	}
+
+	IEnumerator PowerUpBar () {
 		while (healthBar.fillAmount < 1)
 		{
-			healthBar.fillAmount 
-			+= barTime; 
+			healthBar.fillAmount += amountToAdd; 
+			yield return new WaitForSeconds(barTime);
+		}
+	}
+
+	IEnumerator PowerDownBar () {
+		float tempAmount = powerLevel;
+		float fillAmount = healthBar.fillAmount; 
+		while (tempAmount > 0)
+		{
+			 fillAmount = tempAmount - amountToAdd;
+			 healthBar.fillAmount = fillAmount;  
 			yield return new WaitForSeconds(barTime);
 		}
 	}	
-
 }
