fe41de9f12f1f12319d475a188e4f04500b9f809
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-10-30 11:54:10 -0600

    Checkpoint and replay button

---
+++ 1600fall2017/Assets/FinalProject/Scripts/UI/ReplayGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ReplayGame : MonoBehaviour {
+
+	public Transform player; 
+	public UI_Health_Bar uiBar; 
+	public GameObject GameOverUI;
+
+	public static Vector3 startPosition; 
+	private float fillAmount; 
+
+	void Start()
+	{
+		startPosition = player.position; 
+		fillAmount = uiBar.healthBar.fillAmount; 
+		GameOverUI.SetActive(false);	
+	} 
+
+	public void Click () {
+		CharacterControl.gameOver = false; 
+		player.position = startPosition;
+		uiBar.healthBar.fillAmount = fillAmount; 
+		GameOverUI.SetActive(false);
+	}
+}
