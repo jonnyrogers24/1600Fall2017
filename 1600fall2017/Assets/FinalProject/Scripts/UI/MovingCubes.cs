3bd1babde89d407308ce4fe16fab01840126884b
Unknown <jonnyrogersanimation@gmail.com>
2017-10-31 12:46:54 -0600

    making power down cube move up&down

---
+++ 1600fall2017/Assets/FinalProject/Scripts/UI/MovingCubes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCubes : MonoBehaviour {
+
+	public Transform movingCube;
+	public Transform position1;
+	public Transform position2; 
+	public Vector3 newPosition; 
+	public string currentState; 
+	public float smooth; 
+	public float resetTime; 
+	
+	void Start () {
+		ChangeTarget ();
+	}
+	
+	void FixedUpdate () {
+		movingCube.position = Vector3.MoveTowards (movingCube.position, newPosition, smooth * Time.deltaTime );
+	}
+
+	void ChangeTarget () {
+		if (currentState == "Moving  to Position 1"){
+			currentState = "Moving to Position 2"; 
+			newPosition = position2.position; 
+		}
+		else if (currentState == "Moving to Position 2"){
+			currentState = "Moving to Position 1";
+			newPosition = position1.position; 
+		}
+		else if (currentState == ""){
+			currentState = "Moving to Position 2";
+			newPosition = position2.position;
+		}
+		Invoke ("ChangeTarget", resetTime);
+	}
+}
