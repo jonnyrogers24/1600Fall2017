572870b54cf994121bc83ac5e64418796c499666
Jonny Rogers <jonnyrogersanimation@gmail.com>
2017-11-13 11:54:36 -0700

    Started new project folder, organized assets

---
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/Character/CharacterMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CharacterMove : MonoBehaviour {
+
+	public CharacterController characterController;
+	public static bool gameOver;
+	public float gravity = 9.81f;
+	public float speed = 12;
+	public Vector3 moveVector3; 
+	public float jumpForce = 20;  
+	void Start () {
+
+	}
+
+
+	void Update () {
+		moveVector3.y -= gravity * Time.deltaTime;
+
+		if(characterController.isGrounded && !gameOver)
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				moveVector3.y = jumpForce * Time.deltaTime;
+			}
+			
+		
+			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+			//moveVector3.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		}
+		 
+		characterController.Move(moveVector3);
+
+	}
+}
