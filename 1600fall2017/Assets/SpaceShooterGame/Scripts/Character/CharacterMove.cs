67675049bb7e7cb802b9416cbdf078b74ac15742
Unknown <jonnyrogersanimation@gmail.com>
2017-11-27 12:37:10 -0700

    created lasers for player to shoot and attached the lasers to the player

--- 1600fall2017/Assets/SpaceShooterGame/Scripts/Character/CharacterMove.cs
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/Character/CharacterMove.cs
@@ -9,12 +9,27 @@ public class CharacterMove : MonoBehaviour {
 	public float speed = 12;
 	public Vector3 moveVector3; 
 	public float jumpForce = 20;  
+
+	//laser variables
+	public GameObject laser; 
+	public Transform laserSpawn;
+	public float fireRate;
+	private float nextFire;  
 	void Start () {
 
 	}
 
+	void Update ()
+		{
+			if(Input.GetButton("Fire1") && Time.time > nextFire) 
+			{
+				nextFire = Time.time + fireRate; 
+			//instatiate will give us the new values for the vector3 and the quatornian when the lasers are activated by the player.
+				Instantiate (laser, laserSpawn.position, laserSpawn.rotation);
+			}
+		}
 
-	void Update () {
+	void FixedUpdate () {
 		//moveVector3.y -= gravity * Time.deltaTime;
 
 		//if(characterController.isGrounded && !gameOver)
---
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/LaserMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserMover : MonoBehaviour 
+{
+		public float speed; 
+		void Start () 
+		{
+			GetComponent<Rigidbody>().velocity = transform.right * speed;
+			//transform.right with make the laser move from tghe left of the screen to the right. 
+			//* speed is then multiplied by the transform. in this case the speed is double that of the spaceship
+		}
+}
