2363fbd040368e9c23d3e50a93c99bbc52c427dd
Unknown <jonnyrogersanimation@gmail.com>
2017-11-27 20:55:13 -0700

    Made Asteroids rotate, added explosion when asteroids are destoyed by player

---
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/AsteroidDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidDestroy : MonoBehaviour 
+{
+	public GameObject explosion; 
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "BoundaryBox")
+		{
+			return;   
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/RandomRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour 
+{
+	public float rotate;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * rotate; 
+	}
+
+}
