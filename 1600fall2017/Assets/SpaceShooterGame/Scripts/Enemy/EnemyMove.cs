acedd94b850c905530f154b5619c278f58ba8c24
Unknown <jonnyrogersanimation@gmail.com>
2017-12-01 11:57:25 -0700

    Project Saved

--- 1600fall2017/Assets/SpaceShooterGame/Scripts/Enemy/EnemyMove.cs
+++ 1600fall2017/Assets/SpaceShooterGame/Scripts/Enemy/EnemyMove.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
 
-	private CharacterController spaceShip; 
+	private GameObject spaceShip; 
 	public float moveSpeed; 
-	public float playerRange;
-	//public LayerMask playerLayer; 
-	//public bool spaceshipInRange; 
-	void Start () {
-		
 
-		//because the spaceship(player) has a CharacterController, the enemy is trying to find it. 
-		spaceShip = FindObjectOfType<CharacterController>();
-	}
-	
-	void Update () {
-		//spaceshipInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
-		//if(spaceshipInRange)
+	void Start()
 		{
-		//the following is what tells th enemy player to reach, which are the data from the spaceship(player)
+			spaceShip = GameObject.FindGameObjectWithTag("Player");
+		}
+	void Update () 
+	{
+		{
+		//the following is what tells the enemy player to reach, which is the data from the spaceship(player)
 		transform.position = Vector3.MoveTowards (transform.position, spaceShip.transform.position, moveSpeed * Time.deltaTime);
 		}
 	}	
-
-	//void OnDrawGizmosSelected()
-	//{
-	//	Gizmos.DrawSphere(transform.position, playerRange);
-	//}
 }
